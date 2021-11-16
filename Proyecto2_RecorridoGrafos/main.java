f857673aace2ffdcfb050d0caabd0c84d6967058
EAncoHuaracha <eancohu@unsa.edu.pe>
2021-11-16 10:08:27 -0500

    se modifico el main

--- Proyecto2_RecorridoGrafos/main.java
+++ Proyecto2_RecorridoGrafos/main.java
@@ -15,6 +15,29 @@ public class main {
         // guardado de imagen
         BufferedImage img = obj.imprimirImagen();
         ImageIO.write(img, "jpg", new File("C:\\Users\\erick\\eclipse-workspace\\2B\\src\\Proyecto2_RecorridoGrafos\\images\\Salidaimagen1.jpg"));
-  
+        
+        int matrizImg [][]= obj.MatrizImg(img);
+
+		//Impresion de matriz
+        mostrarMatriz(matrizImg);
+        
+        BreadFirstSearch objBFS = new BreadFirstSearch();
+        objBFS.BFS(matrizImg);
+        
+        mostrarMatriz(matrizImg);
+        
+        DepthFirstSearch objDFS = new DepthFirstSearch();
+        objDFS.DFS(matrizImg, 0, 0, 0);   
+        
+        mostrarMatriz(matrizImg);
+    }
+    
+    public static void mostrarMatriz (int [][] matrizImg) {
+    	 for (int i = 0; i < matrizImg.length; i++) {
+ 			for (int j = 0; j < matrizImg[i].length; j++) {
+ 				System.out.print("(" + matrizImg[i][j] + ")\t");
+ 			}
+ 			System.out.println("");
+ 		} 
     }
 }
